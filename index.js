ea89fd3ac7a1d2aadd838c7d9a95a9cfd5ea9326
ibrokhimovnba <ibrokhimovnba@gmail.com>
2025-11-29 17:21:16 +0500

    Regex

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const elForm = document.querySelector(".form");
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()\-_=+{}\[\]:;"'<>.,?/~]).{8,}$/;
-
+const usernameRegex = /^[a-zA-Z0-9]{5,15}$/;
 
 elForm.addEventListener("submit", (e)=> {
     e.preventDefault();
@@ -10,30 +10,94 @@ elForm.addEventListener("submit", (e)=> {
     const password = elForm["password"].value.trim();
     const email = elForm["email"].value.trim();
 
-    // const user = {
-    //     username: username,
-    //     password: password,
-    //     email: email,
-    // };
+    if(!emailRegex.test(email)) {           //  email 
+       return  Toastify({
+        text: "Invalid email address",
+        duration: 2000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+        background: "linear-gradient(to right, red, crimson)",
+     },
+        onClick: function(){} // Callback after click
+     }).showToast();
+    }
 
 
-    
+    if(!passwordRegex.test(password)) {       //  password
+       return  Toastify({
+        text: "Invalid password",
+        duration: 2000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+        background: "linear-gradient(to right, red, crimson)",
+     },
+        onClick: function(){} // Callback after click
+     }).showToast();
+    }
 
+
+    if(username === "") {                      //  username
+      return Toastify({
+        text: "Bo'sh maydonni to'ldiring ! ",
+        duration: 2000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+        background: "linear-gradient(to right, red, crimson)",
+     },
+        onClick: function(){} // Callback after click
+     }).showToast();
+    }
+
+    if(!usernameRegex.test(username)) {
+      return Toastify({
+        text: "Username 5-15 harf va raqamdan iborat bo'lishi kerak ! ",
+        duration: 2000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top", // `top` or `bottom`
+        position: "center", // `left`, `center` or `right`
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        style: {
+        background: "linear-gradient(to right, red, crimson)",
+     },
+        onClick: function(){} // Callback after click
+     }).showToast();
+    }
+
+    const user = {
+        username: username,
+        password: password,
+        email: email,
+    };
 
     Toastify({
-  text: "Hello World",
-  duration: 3000,
-  destination: "https://github.com/apvarun/toastify-js",
-  newWindow: true,
-  close: true,
-  gravity: "top", // `top` or `bottom`
-  position: "left", // `left`, `center` or `right`
-  stopOnFocus: true, // Prevents dismissing of toast on hover
-  style: {
-    background: "linear-gradient(to right, #00b09b, #96c93d)",
-  },
-  onClick: function(){} // Callback after click
-}).showToast();
-
-    console.log(user);
-});
+      text: "Tizimga muvaffaqiyatli kirildi ! ",
+      duration: 2000,
+      destination: "https://github.com/apvarun/toastify-js",
+      newWindow: true,
+      close: true,
+      gravity: "top", // `top` or `bottom`
+      position: "center", // `left`, `center` or `right`
+      stopOnFocus: true, // Prevents dismissing of toast on hover
+      style: {
+        background: "linear-gradient(to right, green, greenyellow)",
+      },
+     onClick: function(){} // Callback after click
+    }).showToast();
+});      
