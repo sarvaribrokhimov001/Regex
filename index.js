728bd88fc7e52843a866bc90986b2cd72f22a352
ibrokhimovnba <ibrokhimovnba@gmail.com>
2025-11-29 12:34:11 +0500

    Regex

---
+++ index.js
@@ -0,0 +1,12 @@
+const elForm = document.querySelector(".form");
+
+
+elForm.addEventListener("submit", (e)=> {
+    e.preventDefault();
+
+    console.log(elForm);
+});
+
+
+
+// console.log(elForm);
